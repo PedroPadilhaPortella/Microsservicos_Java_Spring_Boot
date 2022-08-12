8d484596259a570353c28d8d7169b5a4a188bfbe
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-08-11 23:28:07 -0300

    Project hr-payroll created

---
+++ hr-payroall/src/main/java/com/devsuperior/hrpayroall/HrPayroallApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayroallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayroallApplication.class, args);
+	}
+
+}
---
+++ hr-payroall/src/test/java/com/devsuperior/hrpayroall/HrPayroallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayroallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
